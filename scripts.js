f17bbbb5f18b4a8422ad38d74861cf5de12a0636
felippedaniel <seu-email>
2024-03-15 15:27:50 -0300

    Alterações no projeto

--- scripts.js
+++ scripts.js
@@ -1,23 +1,69 @@
 
 const list = document.querySelector('ul')
 const buttonShowAll = document.querySelector('.show-all')
+const buttonMapAll = document.querySelector('.map-all')
+const buttonSumAll = document.querySelector('.sum-all')
+const buttonFilterAll = document.querySelector('.filter-all')
 
-let myLi = ''
+function formatCurrency(value) {
+    const newValue = value.toLocaleString('pt-br', {
+        style: 'currency',
+        currency: 'BRL',
 
-function buttonShowAll() {
-    menuOptions.forEach((product) => {
+    })
+
+    return newValue
+}
+
+function showAll(productsArray) {
+
+    let myLi = ''
+    productsArray.forEach((product) => {
         myLi += `
             <li>
                 <img src=${product.src}>
                 <p>${product.name}</p>
-                <p class="item-price">R$ ${product.price}</p>
+                <p class="item-price">R$ ${formatCurrency(product.price)}</p>
             </li>
-           `
-    })
+        `;
+    });
 
-    list.innerHTML = myLi
+    list.innerHTML = myLi;
+}
+function mapAllItens() {
+    const newPrices = menuOptions.map((product) => ({
+        ...product,
+        price: product.price * 0.9,
+
+    }))
+
+    showAll(newPrices)
+    // Spread operator
 
 }
 
+function sumAllItens() {
+    const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
 
-buttonShowAll.addEventListener('click', showAll)
+    list.innerHTML = `
+
+          
+            <li>
+                <p>O valor total dos itens é R$ ${formatCurrency(totalValue)}</p>
+            </li>
+          `
+
+}
+
+function filterAllItens() {
+
+    const filterJustVegan = menuOptions.filter((product) => product.vegan)
+
+    showAll(filterJustVegan)
+}
+
+
+buttonShowAll.addEventListener('click', () => showAll(menuOptions))
+buttonMapAll.addEventListener('click', mapAllItens)
+buttonSumAll.addEventListener('click', sumAllItens)
+buttonFilterAll.addEventListener('click', filterAllItens)
